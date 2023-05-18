22fbe017e2888e3faf1b1473000a93124a54e7fd
Roman Godun <godun_ra@astralai.com>
2023-05-18 22:02:03 +0300

    fix: pre-commit

--- config/utils.py
+++ config/utils.py
@@ -3,34 +3,28 @@ import sys
 from pathlib import Path
 from typing import Optional
 from datetime import datetime
-from black_interface.utils import get_env_dict, get_logger
 from dotenv import dotenv_values
-from loguru import logger
+from loguru import logger as base_logger
 from functools import wraps
+
 
 def get_env_dict(env_dir: Optional[Path]):
     env_shared = dotenv_values(str(env_dir / "shared.env"))
     env_secret = dotenv_values(str(env_dir / "secret.env"))
     env_dict = {**env_shared, **env_secret, **os.environ}
-    env_dict = {
-        k: v
-        for k, v in env_dict.items()
-        if ("<secret>" not in v) or ("<project>" not in v)
-    }
+    env_dict = {k: v for k, v in env_dict.items() if ("<secret>" not in v) or ("<project>" not in v)}
     return env_dict
 
 
-def get_logger(
-    logging_level: str, log_dir: Optional[Path] = None, logging_dir_level: str = "DEBUG"
-):
-    logger.remove()
-    logger.add(sys.stderr, level=logging_level)
+def get_logger(logging_level: str, log_dir: Optional[Path] = None, logging_dir_level: str = "DEBUG"):
+    base_logger.remove()
+    base_logger.add(sys.stderr, level=logging_level)
 
     # если указать путь, то логи будут записываться не только в std.err
     if log_dir:
-        logger.add(log_dir / "{time}.log", level=logging_dir_level)
+        base_logger.add(log_dir / "{time}.log", level=logging_dir_level)
 
-    return logger
+    return base_logger
 
 
 env_dict = get_env_dict(Path(__file__).parent)
@@ -41,18 +35,20 @@ logger = get_logger(
 )
 
 
-def logger_dec(func_info:str) -> callable:
-    def decorator(func) -> callable:
+def logger_dec(func_info: str) -> callable:
+    def decorator(func: callable) -> callable:
         @wraps(func)
         def wrapper(*args, **kwargs):
-            
+
             start_time = datetime.now()
             logger.info(f"start {func_info}")
-            
+
             res = func(*args, **kwargs)
-            
+
             logger.info(f"finish {func_info} ({datetime.now() - start_time})")
-            
+
             return res
+
         return wrapper
+
     return decorator
